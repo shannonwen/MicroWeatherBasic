0f1e6e5008d60df251250919dec4ccf02095fb82
shannonwen <wenlongxiang@live.com>
2019-05-27 21:55:29 +0800

    create

---
+++ src/main/java/com/wenlongxiang/microweatherbasic/microweatherbasic/MicroweatherbasicApplication.java
@@ -0,0 +1,13 @@
+package com.wenlongxiang.microweatherbasic.microweatherbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroweatherbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroweatherbasicApplication.class, args);
+	}
+
+}
